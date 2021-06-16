04c3fd2fff74631e9f8dbf2f0ed9ac3334b2daea
sumeragizzz <developer@sumeragizzz.dev>
2021-06-16 12:44:04 +0900

    AC ABC073B

---
+++ src/ABC/073/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        cin >> l >> r;
+
+        sum += r - l + 1;
+    }
+
+    cout << sum << endl;
+}
