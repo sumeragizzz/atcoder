241c396e6af1982c57da4facc00641d4a402bb0a
sumeragizzz <developer@sumeragizzz.dev>
2021-06-21 15:33:37 +0900

    AC APG4b EX17

---
+++ src/APG4b/EX17.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, S;
+    cin >> N >> S;
+    vector<int> A(N), P(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> P.at(i);
+    }
+
+    // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+    // ここにプログラムを追記
+
+    int ans = 0;
+    for (int a : A) {
+        for (int p : P) {
+            if (a + p == S) {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
