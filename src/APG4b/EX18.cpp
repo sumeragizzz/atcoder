e9a00073f6f8239629341fae9524dcd7fbffa34c
sumeragizzz <developer@sumeragizzz.dev>
2021-06-21 16:31:46 +0900

    AC APG4b EX18

---
+++ src/APG4b/EX18.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++) {
+        cin >> A.at(i) >> B.at(i);
+    }
+
+    // ここにプログラムを追記
+    // (ここで"試合結果の表"の2次元配列を宣言)
+
+    vector<vector<char>> ans(N, vector<char>(N, '-'));
+    for (int i = 0; i < M; i++) {
+        ans.at(A.at(i) - 1).at(B.at(i) - 1) = 'o';
+        ans.at(B.at(i) - 1).at(A.at(i) - 1) = 'x';
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << ans.at(i).at(j);
+            if (j < N - 1) {
+                cout << ' ';
+            } else {
+                cout << endl;
+            }
+        }
+    }
+}
