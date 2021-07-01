99f611309b6d24e3e0d9907e7a6815166ef34d1a
sumeragizzz <developer@sumeragizzz.dev>
2021-07-01 20:54:01 +0900

    AC ABC139C

--- src/ABC/139/C.cpp
+++ src/ABC/139/C.cpp
@@ -12,8 +12,18 @@ int main() {
 
     int ans = 0;
     for (int i = 0; i < n; i++) {
+        // 移動できる回数が現時点の最大回数以下の場合は終了
+        if (n - i - 1 <= ans) {
+            break;
+        }
         int count = 0;
+        // 現時点の最大回数を超える為に移動すべき最低の位置
+        int target = i + ans + 1;
         for (int j = i; j < n - 1; j++) {
+            // 対象の位置より左であるのに既に値が低かった場合は終了
+            if (target < n && j < target && h.at(j) < h.at(target)) {
+                break;
+            }
             if (h.at(j) < h.at(j + 1)) {
                 break;
             }
