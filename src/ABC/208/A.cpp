bad91dd9718f89aef93d114a26779cc21d731875
sumeragizzz <developer@sumeragizzz.dev>
2021-07-05 09:01:34 +0900

    AC ABC208A

---
+++ src/ABC/208/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (1 * a <= b && 6 * a >= b) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
