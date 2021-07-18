62f16a0ea4f7a3c15b773a0cdbdeccbfd5b15c3d
sumeragizzz <developer@sumeragizzz.dev>
2021-07-18 20:54:25 +0900

    AC ABC210C

--- src/ABC/210/C.cpp
+++ src/ABC/210/C.cpp
@@ -5,20 +5,19 @@ int main() {
     int n, k;
     cin >> n >> k;
     vector<int> c(n);
-    for (int i = 0; i < n; i++) {
-        cin >> c.at(i);
-    }
+    for (int i = 0; i < n; i++) cin >> c.at(i);
 
+    map<int, int> m;
     int ans = 0;
-    for (int i = 0; i < n - k + 1; i++) {
-        set<int> s;
-        for (int j = 0; j < k; j++) {
-            s.insert(c.at(i + j));
+    for (int i = 0; i < n; i++) {
+        m[c.at(i)]++;
+        if (i >= k) {
+            m[c.at(i - k)]--;
+            if (m[c.at(i - k)] == 0) {
+                m.erase(c.at(i - k));
+            }
         }
-        ans = max(ans, (int)s.size());
-        if (ans == k) {
-            break;
-        }
+        ans = max(ans, (int)m.size());
     }
     cout << ans << endl;
 }
