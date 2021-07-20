2f9f32fed7a98fe885ce2b78aad335baa7b67ad6
sumeragizzz <developer@sumeragizzz.dev>
2021-07-20 13:44:40 +0900

    AC ABC049B

---
+++ src/ABC/049/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<string> c(h);
+    for (int i = 0; i < h; i++) {
+        cin >> c.at(i);
+    }
+
+    for (int i = 0; i < h; i++) {
+        cout << c.at(i) << endl;
+        cout << c.at(i) << endl;
+    }
+}
