8cbec48f886f06f762735bd13cd74eb9baaccd98
sumeragizzz <developer@sumeragizzz.dev>
2021-07-26 23:35:06 +0900

    AC ABC075A

---
+++ src/ABC/075/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (b == c) cout << a << endl;
+    if (a == c) cout << b << endl;
+    if (a == b) cout << c << endl;
+}
