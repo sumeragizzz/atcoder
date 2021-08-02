80f4e97ac747d0ce4a6051443cc20ea9ef5f8c64
sumeragizzz <developer@sumeragizzz.dev>
2021-08-02 22:35:30 +0900

    AC ABC090A

---
+++ src/ABC/090/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        string c;
+        cin >> c;
+        cout << c.at(i);
+    }
+    cout << endl;
+}
